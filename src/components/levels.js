50c981350371ae1254ffe6862cb6aa004a1cdba2
mrjackmaster <jacektrg+github@gmail.com>
2021-09-19 16:58:17 +0200

    Updating levels with URLs to firebase files.

--- src/components/levels.js
+++ src/components/levels.js
@@ -1,27 +1,20 @@
-import picture1 from '../assets/wheres-waldo-1.jpg';
-import picture2 from '../assets/wheres-waldo-2.jpg';
-import picture3 from '../assets/wheres-waldo-3.jpg';
-import picture4 from '../assets/wheres-waldo-4.jpg';
-
-import Waldo from '../assets/Waldo.jpg';
-import Odlaw from '../assets/Odlaw.jpg';
-import Wizard from '../assets/Wizard.jpg';
-import Wenda from '../assets/Wenda.jpg';
-
 const levels = [
 	{
 		id: 'Level 1',
-		picture: picture1,
+		pictureURL:
+			'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fpictures%2Fwheres-waldo-1.jpg?alt=media&token=6819b9be-b266-453f-9a4d-dd6391d433ef',
 		characters: [
 			{
 				name: 'Waldo',
-				photo: Waldo,
+				photoURL:
+					'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fcharacters%2FWaldo.jpg?alt=media&token=2369c7e6-c61a-4a16-97e6-71f2016ea59f',
 				coords: { X: 1125, Y: 95 },
 				found: false,
 			},
 			{
 				name: 'Odlaw',
-				photo: Odlaw,
+				photoURL:
+					'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fcharacters%2FOdlaw.jpg?alt=media&token=22061823-e0c5-4327-90f0-a69f7ab6054c',
 				coords: { X: 70, Y: 770 },
 				found: false,
 			},
@@ -29,23 +22,27 @@ const levels = [
 	},
 	{
 		id: 'Level 2',
-		picture: picture2,
+		pictureURL:
+			'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fpictures%2Fwheres-waldo-2.jpg?alt=media&token=faed3502-47a3-449b-8c03-180b3cfebee9',
 		characters: [
 			{
 				name: 'Waldo',
-				photo: Waldo,
+				photoURL:
+					'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fcharacters%2FWaldo.jpg?alt=media&token=2369c7e6-c61a-4a16-97e6-71f2016ea59f',
 				coords: { X: 670, Y: 220 },
 				found: false,
 			},
 			{
 				name: 'Wenda',
-				photo: Wenda,
+				photoURL:
+					'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fcharacters%2FWenda.jpg?alt=media&token=15a8ce9f-bb44-4c22-9589-f26b71170294',
 				coords: { X: 315, Y: 335 },
 				found: false,
 			},
 			{
 				name: 'Wizard',
-				photo: Wizard,
+				photoURL:
+					'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fcharacters%2FWizard.jpg?alt=media&token=42729e91-0a4f-46bc-a171-1f9530185e04',
 				coords: { X: 970, Y: 175 },
 				found: false,
 			},
@@ -53,17 +50,20 @@ const levels = [
 	},
 	{
 		id: 'Level 3',
-		picture: picture3,
+		pictureURL:
+			'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fpictures%2Fwheres-waldo-3.jpg?alt=media&token=1d19f902-f6bd-4eb3-bd1f-1706e24601d4',
 		characters: [
 			{
 				name: 'Waldo',
-				photo: Waldo,
+				photoURL:
+					'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fcharacters%2FWaldo.jpg?alt=media&token=2369c7e6-c61a-4a16-97e6-71f2016ea59f',
 				coords: { X: 695, Y: 355 },
 				found: false,
 			},
 			{
 				name: 'Wenda',
-				photo: Wenda,
+				photoURL:
+					'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fcharacters%2FWenda.jpg?alt=media&token=15a8ce9f-bb44-4c22-9589-f26b71170294',
 				coords: { X: 1005, Y: 400 },
 				found: false,
 			},
@@ -71,11 +71,13 @@ const levels = [
 	},
 	{
 		id: 'Level 4',
-		picture: picture4,
+		pictureURL:
+			'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fpictures%2Fwheres-waldo-4.jpg?alt=media&token=56ad751c-2c50-4246-9b7a-018bae04df4d',
 		characters: [
 			{
 				name: 'Wenda',
-				photo: Wenda,
+				photoURL:
+					'https://firebasestorage.googleapis.com/v0/b/where-s-waldo-1ebf7.appspot.com/o/assets%2Fcharacters%2FWenda.jpg?alt=media&token=15a8ce9f-bb44-4c22-9589-f26b71170294',
 				coords: { X: 1060, Y: 765 },
 				found: false,
 			},
