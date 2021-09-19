2a8e3ee4877b8d53aecec7c9e6275e8d62998e65
mrjackmaster <jacektrg+github@gmail.com>
2021-09-19 16:55:37 +0200

    Gettin level info from firestore docs.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { app, db } from './firebase';
 import Home from './components/Home';
 import Game from './components/Game';
-import allLevels from './components/levels';
+import app from './firebase';
+// import levelsFile from './components/levels';
+import { getDocs, collection, getFirestore } from 'firebase/firestore';
 
 const App = () => {
+	const [allLevelsData, setAllLevelsData] = useState([]);
 	const [levelData, setLevelData] = useState();
 
-	const goToLevel = (e) => {
-		const { id } = e.currentTarget;
-		setLevelData(allLevels.find((level) => level.id === id));
+	const goToLevel = (id) => {
+		setLevelData(allLevelsData.find((level) => level.id === id));
 	};
+
+	useEffect(() => {
+		localStorage.clear();
+
+		// levelsFile.forEach((level) => {
+		// 	setDoc(doc(getFirestore(), 'gameLevels', level.id), {
+		// 		...level,
+		// 	});
+		// });
+
+		(async () => {
+			try {
+				const files = await getDocs(
+					collection(getFirestore(), 'gameLevels')
+				).then((files) => {
+					const array = [];
+					files.forEach((file) => {
+						array.push(file.data());
+					});
+					return array;
+				});
+				setAllLevelsData(files);
+			} catch (error) {
+				console.log(
+					'Error reading data from Firebase Database: ',
+					error
+				);
+			}
+		})();
+	}, []);
 
 	return (
 		<BrowserRouter>
 			<Switch>
 				<Route exact path='/'>
-					<Home allLevels={allLevels} goToLevel={goToLevel} />
+					<Home allLevelsData={allLevelsData} goToLevel={goToLevel} />
 				</Route>
 				<Route exact path='/game'>
-					<Game levelData={levelData} />
+					<Game data={levelData} />
 				</Route>
 				<Redirect to='/' />
 			</Switch>
