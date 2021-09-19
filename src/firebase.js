d544486b4adc64429d7486c1a74f3acf8b52bd47
mrjackmaster <jacektrg+github@gmail.com>
2021-09-19 16:55:47 +0200

    Firebase config file.

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const config = {
+	apiKey: 'AIzaSyBXoP0Ew1xEsltvwsM9QhqdUJFQW_8UPpE',
+	authDomain: 'where-s-waldo-1ebf7.firebaseapp.com',
+	projectId: 'where-s-waldo-1ebf7',
+	storageBucket: 'where-s-waldo-1ebf7.appspot.com',
+	messagingSenderId: '422879222896',
+	appId: '1:422879222896:web:f6d62ad28a12a6f577a8b5',
+};
+
+const app = initializeApp(config);
+
+export default app;
