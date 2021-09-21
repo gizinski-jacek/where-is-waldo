6b0388385d24f62ebcc191946269dfd00e743cbc
mrjackmaster <jacektrg+github@gmail.com>
2021-09-21 22:03:47 +0200

    Getting sorted data from server and displaying it in a table.

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -10,7 +10,7 @@ import {
 } from 'firebase/firestore';
 
 const Leaderboard = () => {
-	const [leaderboardData, setLeaderboardData] = useState();
+	const [leaderboardData, setLeaderboardData] = useState([]);
 	const [sortByLevel, setSortByLevel] = useState();
 
 	const fnSetSortByLevel = (e) => {
@@ -25,16 +25,16 @@ const Leaderboard = () => {
 					const docQuery = query(
 						collection(getFirestore(), 'usersGames'),
 						where('levelData.id', '==', sortByLevel),
-						// orderBy('timeElapsed', 'desc'),
-						// orderBy('gameStart', 'desc'),
-						limit(5)
+						orderBy('gameTime', 'asc'),
+						orderBy('gameEnd', 'asc'),
+						orderBy('gameStart', 'asc'),
+						limit(15)
 					);
 					await getDocs(docQuery).then((docs) => {
 						const array = [];
 						docs.forEach((doc) => {
 							array.push(doc.data());
 						});
-						console.log(array);
 						setLeaderboardData(array);
 					});
 				} catch (error) {
@@ -47,25 +47,53 @@ const Leaderboard = () => {
 		}
 	}, [sortByLevel]);
 
-	const display = leaderboardData?.map((entry) => {
-		return <div className='player'></div>;
+	const display = leaderboardData?.map((entry, index) => {
+		console.log(entry.gameEnd);
+		return (
+			<tr key={index} className={`player  no${index + 1}`}>
+				<td className='playerRank'>{'#' + (index + 1)}</td>
+				<td className='playerName'>{entry.playerName}</td>
+				<td className='playerTime'>
+					{new Date(entry.gameTime).toISOString().substr(14, 7)}
+					{/* Version without milliseconds */}
+					{/* {new Date(entry.gameTime).toISOString().substr(14, 5)} */}
+				</td>
+				<td className='playerDatePlayed'>
+					{new Date(entry.gameStart.seconds * 1000)
+						.toLocaleString()
+						.substr(0, 17)}
+				</td>
+			</tr>
+		);
 	});
 
 	return (
 		<div className='leaderboard'>
-			<button id='Level 1' onClick={fnSetSortByLevel}>
-				Level 1
-			</button>
-			<button id='Level 2' onClick={fnSetSortByLevel}>
-				Level 2
-			</button>
-			<button id='Level 3' onClick={fnSetSortByLevel}>
-				Level 3
-			</button>
-			<button id='Level 4' onClick={fnSetSortByLevel}>
-				Level 4
-			</button>
-			fsdfasdf
+			<div className='leaderboardControls'>
+				<button id='Level 1' onClick={fnSetSortByLevel}>
+					Level 1
+				</button>
+				<button id='Level 2' onClick={fnSetSortByLevel}>
+					Level 2
+				</button>
+				<button id='Level 3' onClick={fnSetSortByLevel}>
+					Level 3
+				</button>
+				<button id='Level 4' onClick={fnSetSortByLevel}>
+					Level 4
+				</button>
+			</div>
+			<table>
+				<tbody>
+					<tr>
+						<th style={{ width: '75px' }}>Place</th>
+						<th style={{ width: '200px' }}>Name</th>
+						<th style={{ width: '100px' }}>Time</th>
+						<th style={{ width: '175px' }}>Date Played</th>
+					</tr>
+					{display}
+				</tbody>
+			</table>
 		</div>
 	);
 };
