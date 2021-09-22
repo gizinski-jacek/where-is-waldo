96afa293f6597d2bf2c6dd2e4b27970273cdd96a
mrjackmaster <jacektrg+github@gmail.com>
2021-09-22 20:12:52 +0200

    Moving render of table rows data  to separate component. Replacing button elements with Buttons component.

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -8,8 +8,12 @@ import {
 	orderBy,
 	limit,
 } from 'firebase/firestore';
+import Button from './Button';
+import TableData from './utils/TableData';
 
-const Leaderboard = () => {
+const Leaderboard = (props) => {
+	const { show, toggle } = props;
+
 	const [leaderboardData, setLeaderboardData] = useState([]);
 	const [sortByLevel, setSortByLevel] = useState();
 
@@ -47,53 +51,51 @@ const Leaderboard = () => {
 		}
 	}, [sortByLevel]);
 
-	const display = leaderboardData?.map((entry, index) => {
-		console.log(entry.gameEnd);
-		return (
-			<tr key={index} className={`player  no${index + 1}`}>
-				<td className='rankColumn'>{'#' + (index + 1)}</td>
-				<td className='nameColumn'>{entry.playerName}</td>
-				<td className='timeColumn'>
-					{new Date(entry.gameTime).toISOString().substr(14, 7)}
-					{/* Version without milliseconds */}
-					{/* {new Date(entry.gameTime).toISOString().substr(14, 5)} */}
-				</td>
-				<td className='datePlayedColumn'>
-					{new Date(entry.gameStart.seconds * 1000)
-						.toLocaleString()
-						.substr(0, 17)}
-				</td>
-			</tr>
-		);
+	const tableContent = leaderboardData?.map((entry, index) => {
+		return <TableData key={index} data={entry} index={index} />;
 	});
 
 	return (
-		<div className='leaderboard'>
-			<div className='leaderboardControls'>
-				<button id='Level 1' onClick={fnSetSortByLevel}>
-					Level 1
-				</button>
-				<button id='Level 2' onClick={fnSetSortByLevel}>
-					Level 2
-				</button>
-				<button id='Level 3' onClick={fnSetSortByLevel}>
-					Level 3
-				</button>
-				<button id='Level 4' onClick={fnSetSortByLevel}>
-					Level 4
-				</button>
+		<div
+			className='modalLeaderboard'
+			style={{ display: show }}
+			onClick={toggle}
+		>
+			<div className='leaderboard'>
+				<div className='leaderboardControls'>
+					<Button
+						id='Level 1'
+						func={fnSetSortByLevel}
+						text={'Level 1'}
+					/>
+					<Button
+						id='Level 2'
+						func={fnSetSortByLevel}
+						text={'Level 2'}
+					/>
+					<Button
+						id='Level 3'
+						func={fnSetSortByLevel}
+						text={'Level 3'}
+					/>
+					<Button
+						id='Level 4'
+						func={fnSetSortByLevel}
+						text={'Level 4'}
+					/>
+				</div>
+				<table>
+					<tbody>
+						<tr>
+							<th className='rankColumn'>Place</th>
+							<th className='nameColumn'>Name</th>
+							<th className='timeColumn'>Time</th>
+							<th className='datePlayedColumn'>Date Played</th>
+						</tr>
+						{tableContent}
+					</tbody>
+				</table>
 			</div>
-			<table>
-				<tbody>
-					<tr>
-						<th className='rankColumn'>Place</th>
-						<th className='nameColumn'>Name</th>
-						<th className='timeColumn'>Time</th>
-						<th className='datePlayedColumn'>Date Played</th>
-					</tr>
-					{display}
-				</tbody>
-			</table>
 		</div>
 	);
 };
