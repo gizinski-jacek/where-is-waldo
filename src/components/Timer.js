e5e98b18d2da32b99a617f637f5dcc400922d5ff
mrjackmaster <jacektrg+github@gmail.com>
2022-04-23 19:36:13 +0200

    Cleanup

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import Game from './Game';
 
 const Timer = (props) => {
 	const { stop, startDate, fnSetTime } = props;
@@ -16,26 +15,5 @@ const Timer = (props) => {
 	});
 	return null;
 };
-
-//
-// Version without milliseconds.
-// Needs changes in component Game.js at line (150).
-//
-// const Timer = (props) => {
-// 	const { stop, startDate, fnSetTime } = props;
-// 	useEffect(() => {
-// 		if (!stop) {
-// 			const timer = setInterval(() => {
-// 				const newTime = Math.floor(Date.now() - startDate) / 1000;
-// 				fnSetTime(newTime);
-// 			}, 1000);
-// 			return () => {
-// 				clearInterval(timer);
-// 			};
-// 		}
-// 	});
-// 	return null;
-// };
-//
 
 export default Timer;
